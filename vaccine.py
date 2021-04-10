fe5ce58e873329c4735c69c64a64c6328321c7d0
Scott-Larsen <scott@scottlarsen.com>
2021-04-10 13:08:19 -0400

    Add logging and sort results by distance when printing

--- vaccine.py
+++ vaccine.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import pickle
 import requests
 import googlemaps
@@ -12,7 +13,7 @@ from dateutil import parser
 from config import API_KEY
 
 
-# A street address or latitude, longitude around which to search
+# A street address or latitude, longitude around which to search.
 SEARCH_ADDRESS = "2849 Street Rd, Doylestown, PA 18902"
 STATE = "PA"
 
@@ -22,32 +23,39 @@ MAX_DISTANCE = 15
 SECONDS_BETWEEN_SCANS = 60
 URL = ["https://www.vaccinespotter.org/api/v0/states/", ".json"]
 
+logging.basicConfig(
+    filename="vaccine.log", encoding="utf-8", filemode="w", level=logging.INFO
+)
+
 # Geolocator settings that don't need to be called each loop
 geolocator = Nominatim(user_agent="scott@scottlarsen.com")
 geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 gmaps = googlemaps.Client(key=API_KEY)
 
 
-def convert_text_address_to_gps(text_address):
+def convert_text_address_to_gps(text_address, geolocations):
     """Converts plain-text street address to (lat, long) tuple."""
 
-    try:  # Try's the free geopy Nominatum geocoder.
-        sleep(1)
-        text_address_geocoded = geocode(text_address)
-        text_address_gps = (
-            text_address_geocoded.latitude,
-            text_address_geocoded.longitude,
-        )
-        print("Successfully geocoded with Geopy")
-        return text_address_gps
+    if text_address not in geolocations:
+        try:  # Try's the free geopy Nominatum geocoder.
+            sleep(1)
+            text_address_geocoded = geocode(text_address)
+            geolocations[text_address] = (
+                text_address_geocoded.latitude,
+                text_address_geocoded.longitude,
+            )
+            logging.info(f"\nGeopy: {text_address}")
 
-    except AttributeError as e:  # Resorts to the Google Maps geocoder on failure
-        print(f"\nQuerying Google for {text_address}")
-        clinic_address = gmaps.geocode(text_address)
-        return (
-            clinic_address[0]["geometry"]["location"]["lat"],
-            clinic_address[0]["geometry"]["location"]["lng"],
-        )
+        except AttributeError as e:  # Resorts to the Google Maps geocoder on failure
+            logging.info(f"\n Google: {text_address}")
+            clinic_address = gmaps.geocode(text_address)
+            geolocations[text_address] = (
+                clinic_address[0]["geometry"]["location"]["lat"],
+                clinic_address[0]["geometry"]["location"]["lng"],
+            )
+    else:
+        logging.info(f"Geolocations: {text_address}")
+    return geolocations[text_address]
 
 
 def find_distance(SEARCH_ADDRESS_GPS, clinic_gps):
@@ -64,7 +72,7 @@ def query_vaccinespotter():
 
 def main():
 
-    # Load or create the pickle file to save the dictionary of geolocated addresses.
+    # Load or create the pickle to save the dictionary of geolocated addresses.
     pickle_filename = STATE + "_geolocations.p"
     if os.path.isfile(pickle_filename):
         geolocations = pickle.load(open(pickle_filename, "rb"))
@@ -76,20 +84,17 @@ def main():
     # Resolve SEARCH_ADDRESS to GPS tuple SEARCH_ADDRESS_GPS
     global SEARCH_ADDRESS
     if isinstance(SEARCH_ADDRESS, str):
-        if SEARCH_ADDRESS in geolocations:
-            SEARCH_ADDRESS_GPS = geolocations[SEARCH_ADDRESS]
-        else:
-            SEARCH_ADDRESS_GPS = convert_text_address_to_gps(SEARCH_ADDRESS)
-            geolocations[SEARCH_ADDRESS] = SEARCH_ADDRESS_GPS
+        SEARCH_ADDRESS_GPS = convert_text_address_to_gps(SEARCH_ADDRESS, geolocations)
     elif isinstance(SEARCH_ADDRESS, tuple):
         SEARCH_ADDRESS_GPS = SEARCH_ADDRESS
 
     while True:
 
         datetime_UTC = datetime.utcnow().replace(tzinfo=UTC)
-        print(f"\nRunning at {str(datetime.now())}")
+        print(f"\nRunning at {str(datetime.now())}\n")
 
         locations = query_vaccinespotter()
+        available_now = []
 
         # Check each clinic location from the API
         for location in locations:
@@ -104,14 +109,10 @@ def main():
                     )
                 else:
                     address = f"{street_address} {location_properties['city']}, \
-                    {location_properties['state']} {location_properties['postal_code']}"
+                    {location_properties['state'].strip()} {location_properties['postal_code']}"
 
                 # If we've already geolocated the address, pull GPS coords from pickle.
-                if location_properties["id"] in geolocations:
-                    gps_address = geolocations[location_properties["id"]]
-                else:
-                    gps_address = convert_text_address_to_gps(address)
-                    geolocations[location_properties["id"]] = gps_address
+                gps_address = convert_text_address_to_gps(address, geolocations)
 
                 # Check the distance between search point and clinic GPS.
                 # Add to available_last_time so we don't notify multiple times
@@ -129,17 +130,22 @@ def main():
                         // 60
                     )
 
-                    print(
-                        f"""
-                        {location_properties["name"]}
-                        {location_properties["address"]}
-                        {location_properties["city"]}, PA \
-                            {location_properties["postal_code"]}
-                        {distance_} miles away
-                        {location_properties["url"]}
-                        Updated {time_diff_in_minutes} minutes ago
-                        """
+                    available_now.append(
+                        [
+                            distance_,
+                            f"{location_properties['name']}\n"
+                            + f"{location_properties['address']}\n"
+                            + f"{location_properties['city']}, {STATE}"
+                            + f"{(location_properties['postal_code']).strip()}"
+                            + f"{distance_} miles away\n"
+                            + f"{location_properties['url']}\n"
+                            + f"Updated {time_diff_in_minutes} minutes ago\n",
+                        ]
                     )
+
+        available_now.sort(reverse=True)
+        for site in available_now:
+            print(site[1])
 
         # Cache street address -> GPS translations to pickle file.
         pickle.dump(geolocations, open(pickle_filename, "wb"))
